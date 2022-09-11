b36dde04da7817d39e4c36489d7ddbcff489666c
VitekIT55 <trakmanic80@gmail.com>
2022-09-11 16:59:25 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'polls/static')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
