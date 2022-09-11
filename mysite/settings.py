86cd5ad8cb60c3b6c5e3e4d11ef0ceae4043754f
VitekIT55 <trakmanic80@gmail.com>
2022-09-11 17:19:58 +0300

    1

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 
 import os
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
