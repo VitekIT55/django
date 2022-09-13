7c5ba0d7799e2a9ad01c3e66746c7466bf67c181
VitekIT55 <trakmanic80@gmail.com>
2022-09-13 19:50:37 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
